a9de605f33e8686d9df0d7c5942fd92b24815e82
Bera Bekir <137100409+berabkrbaykal@users.noreply.github.com>
2023-08-08 14:26:33 +0300

    sentence_write.cpp

--- sentence_write.cpp
+++ sentence_write.cpp
@@ -1,17 +1,13 @@
 #include<stdio.h>
-int main(){	
-	char cumle[100];
-	printf("Lütfen cümle giriniz\n");
-	fgets(cumle,100,stdin);
+int main(){     
+        char cumle[100];
+        int i;
+        printf("Lütfen cümle giriniz\n");
+        fgets(cumle,100,stdin);
 
-	int i;
-        int a=0;
-        printf("Lütfen cümleyi kaç defa yazdırmak istediğinizi giriniz\n");
-        scanf("%d",&i);
-
-	while(a<=i)
-	{
-		printf("%s\n",cumle);
-		a++;
-	}
+        for(i=0;cumle[i];i++)
+        {
+                printf("%c\n",cumle[i]);
+        }
 }
+
